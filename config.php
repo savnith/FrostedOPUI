bb1e013bf5a0b85b7e4ae0bd6affb2d0c79347a3
savnith <savnith@gmail.com>
2017-11-21 19:55:38 -0500

    Added a config

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+# FrostedOPUI Configuration File
+
+$pageTitle = "FrostedOP - Portal";
+$pageIcon = "https://savnith.swrv.life/yhg4jfe.jpg";
+
+# Index Page Customizable Containers
+# fa fa-(image) (more info here: LINK)
+
+$containerOne = "Forums";
+$containerOneLink = "forums";
+$containerOneDesc = "The FrostedOP forums.";
+$containerOneImage = "fa fa-list-alt";
+
+$containerTwo = "Logviewer";
+$containerTwoLink = "logviewer";
+$containerTwoDesc = "Only for admins but shows all the server messages.";
+$containerTwoImage = "fa fa-terminal";
+
+# For the last container you can use the httpd or you can link it to something else.
+
+$containerThree = "HTTPD";
+$containerThreeLink = "httpd";
+$containerThreeDesc = "FrostedOPMod HTTPD, has multiple useful things.";
+$containerThreeImage = "fa fa-archive";
+
+# Meta Information
+$metaTitle = "FrostedOP - Portal Page";
+$metaDesc = "The frostedop server portal page.";
+$metaIcon = "https://savnith.swrv.life/yhg4jfe.jpg";
--- index.php
+++ index.php
@@ -1,18 +1,32 @@
+<?php include 'config.php'; ?>
+
+<!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <title>FrostedOP - Portal</title>
         <link rel="stylesheet" type="text/css" href="/css/main.css">
+        <title><?php echo $pageTitle ?></title>
+        <link rel="icon" href="<?php echo $pageIcon ?>" />
+        <meta property="og:title" content="<?php echo $metaTitle ?>" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="<?php echo $metaIcon ?>" />
+        <meta property="og:description" content="<?php echo $metaDesc ?>"/>
     </head>
     <body><div style="height: 100%; width: 100%;">
-            <div onclick="window.location.href = 'forums'" class="linkcontainer" id="conclave">
-                <h1>Forums</h1>The FrostedOP forums.<span class="fa fa-list-alt"></span>
+            <div onclick="window.location.href = '<?php echo $containerOneLink ?>'" class="linkcontainer" id="forums">
+                <h1><?php echo $containerOne ?></h1>
+                <?php echo $containerOneDesc ?>
+                <span class="<?php echo $containerOneImage ?>"></span>
             </div>
-            <div onclick="window.location.href = 'logviewer'" class="linkcontainer" id="parapet">
-                <h1>Logviewer</h1>Only for admins but shows all the server messages.<span class="fa fa-terminal"></span>
+            <div onclick="window.location.href = '<?php echo $containerTwoLink ?>'" class="linkcontainer" id="logviewer">
+                <h1><?php echo $containerTwo ?></h1>
+                <?php echo $containerTwoDesc ?>
+                <span class="<?php echo $containerOneImage ?>"></span>
             </div>
-            <div onclick="window.location.href = 'httpd'" class="linkcontainer" id="cerberus">
-                <h1>HTTPD</h1>FrostedOPMod HTTPD, has multiple useful things.<span class="fa fa-archive"></span>
+            <div onclick="window.location.href = '<?php echo $containerThreeLink ?>'" class="linkcontainer" id="httpd">
+                <h1><?php echo $containerThree ?></h1>
+                <?php echo $containerThreeDesc ?>
+                <span class="<?php echo $containerThreeImage ?>"></span>
             </div>
         </div>
     </body>
